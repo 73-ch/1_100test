82adb648199d05258a391731e7a0162cea68376a
634nami <cigarbox.nami@gmail.com>
2019-06-21 16:04:41 +0900

    Initial Commit

---
+++ 1_100test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1_100test
+//
+//  Created by Kosaku Namikawa on 2019/06/21.
+//  Copyright © 2019 Kosaku Namikawa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
